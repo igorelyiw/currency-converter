707617a2741f1a63ae5d0dc4c78316ab8be46ac6
anta <igorelyiw@gmail.com>
2022-10-17 18:59:22 +0300

    added icons

---
+++ src/features/assets/index.ts
@@ -0,0 +1,7 @@
+import euro from './euro.png';
+import dollar from './dollar.png';
+import europe from './europe.png';
+import ukraine from './ukraine.png';
+import usa from './usa.png';
+
+export { euro, dollar, europe, ukraine, usa };
